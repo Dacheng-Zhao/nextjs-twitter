fde22bda67299743b67b0ddca6f897d3cde28047
Dacheng Zhao <daczhao123@gmail.com>
2022-05-22 17:21:14 -0400

    Add comments

--- pages/api/getComments.ts
+++ pages/api/getComments.ts
@@ -3,7 +3,7 @@ import { groq } from 'next-sanity';
 import { sanityClient } from '../../sanity';
 import { Comment } from '../../typings';
 
-const commentQuery = groq`*[_type=="comment" && reference(*[_type=="tweet" && _id==$tweetId]._id)]{_id, ...} | order(_createdAt desc)`
+const commentQuery = groq`*[_type=="comment" && references(*[_type=="tweet" && _id==$tweetId]._id)]{_id, ...} | order(_createdAt desc)`
 
 type Data = Comment[]
 
@@ -11,6 +11,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     const { tweetId } = req.query;
 
     const comments: Comment[] = await sanityClient.fetch(commentQuery, {tweetId});
-    console.log(comments);
     res.status(200).json(comments)
 }
