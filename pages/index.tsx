43af5c0a47e4cfe16a0b8e62bd711e15aa7304eb
Dacheng Zhao <daczhao123@gmail.com>
2022-05-22 16:10:58 -0400

    Add sanity cms

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Feed from '../components/Feed'
 import SideBar from '../components/SideBar'
 import Widgets from '../components/Widgets'
+import { Tweet } from '../typings'
+import { fetchTweets } from '../utils/fetchTweets'
 
-const Home: NextPage = () => {
+interface Props {
+  tweets: Tweet[]
+}
+
+const Home = ({ tweets }: Props) => {
+
   return (
     <div className="lg:max-w-6xl mx-auto max-h-screen overflow-hidden">
       <Head>
@@ -13,7 +20,7 @@ const Home: NextPage = () => {
       </Head>
       <main className='grid grid-cols-9'>
         <SideBar />
-        <Feed />
+        <Feed tweets={tweets}  />
         <Widgets />
       </main>
     </div>
@@ -21,3 +28,13 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const tweets = await fetchTweets();
+
+  return {
+    props: {
+      tweets,
+    }
+  }
+}
