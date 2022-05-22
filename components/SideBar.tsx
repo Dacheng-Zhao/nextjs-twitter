5c9b73edc17cee692ba68b9500998865d8fb4b24
Dacheng Zhao <daczhao123@gmail.com>
2022-05-21 23:56:28 -0400

    Basic setup for twitter

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { BellIcon, HashtagIcon, BookmarkIcon, CollectionIcon, DotsCircleHorizontalIcon, MailIcon, UserIcon, HomeIcon} from '@heroicons/react/outline'
+import SideBarRow from './SideBarRow'
 
 function SideBar() {
   return (
-    <div>SideBar</div>
+    <div className='flex flex-col col-span-2 items-center px-4 md:items-start'>
+      <img className='h-10 w-10 m-3' src='https://links.papareact.com/drq' alt='' />
+      <SideBarRow Icon={HomeIcon} title='Home' />
+      <SideBarRow Icon={HashtagIcon} title='Explore' />
+      <SideBarRow Icon={BellIcon} title='Notifications' />
+      <SideBarRow Icon={MailIcon} title='Messages' />
+      <SideBarRow Icon={BookmarkIcon} title='Bookmarks' />
+      <SideBarRow Icon={CollectionIcon} title='Lists' />
+      <SideBarRow Icon={UserIcon} title='Sign In' />
+      <SideBarRow Icon={DotsCircleHorizontalIcon} title='More' />
+    </div>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Feed from '../components/Feed'
 import SideBar from '../components/SideBar'
+import Widgets from '../components/Widgets'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="lg:max-w-6xl mx-auto max-h-screen overflow-hidden">
       <Head>
         <title>Twitter 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className='grid grid-cols-9'>
         <SideBar />
+        <Feed />
+        <Widgets />
       </main>
     </div>
   )
